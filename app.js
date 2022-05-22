1bd5b72058aa0e842a8076a544409ecbcd56132b
farman <farmanparvez07@gmail.com>
2022-05-22 18:48:46 +0530

    cores

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRoutes')
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('app is runnning'))
